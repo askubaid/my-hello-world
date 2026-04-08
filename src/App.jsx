ef874637a8440b05861806810fe192b2d82f27a0
Ubaid ur Rehman <askubaid@gmail.com>
2026-04-08 13:22:36 +0500

    initial hello world commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World!</h1>
+      <p>My first React app on </p>
+      </div>
+  )
+}
+
+export default App
